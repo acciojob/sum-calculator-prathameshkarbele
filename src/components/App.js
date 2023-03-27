cd6ae1dccf998183b14396ba8d56132423d5ce7a
Prathamesh karbele <113411026+prathameshkarbele@users.noreply.github.com>
2023-03-27 15:22:20 +0000

    chnges

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
 
+import { useState } from "react";
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
+
+
+const CalC = () => {
+  const [numInput, setNumInputs]=useState([]);//state var
+  const [sum, setSum]=useState(0);
+
+  const handleChange=(e)=>{
+    const inputValue=e.target.value;
+    setNumInputs([...numInput, inputValue]);
+  }
+setTimeout(() => {
+  let localSum=0;
+  numInput.forEach((num)=>{
+    localSum+=Number(num)
+  });
+  setSum(localSum)
+},500);
+
+
   return (
     <div>
+      <input type="number"
+      onChange={handleChange} />
+      <p>Sum of all number: {sum}</p>
         {/* Do not remove the main div */}
     </div>
   )
-}
+};
 
-export default App
+export default CalC;
